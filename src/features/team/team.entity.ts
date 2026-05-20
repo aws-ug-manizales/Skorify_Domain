e5970cbcac0632b9187ae35533336ef6e53bc0af
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2026-05-20 18:10:06 -0500

    Update

--- src/features/team/team.entity.ts
+++ src/features/team/team.entity.ts
@@ -1,20 +1,23 @@
-import { BuiltEntityDomainEvent, DomainEvent } from '../../core';
-import { Entity, Id } from '../../core/entity';
+import { BuiltEntityDomainEvent, DomainEvent } from "../../core";
+import { Entity, Id } from "../../core/entity";
 
 export interface TeamAttributes {
   id: Id;
   name: string;
+  tournamentId: Id;
   shieldUrl?: string;
 }
 
 export class TeamEntity extends Entity {
   name: string;
+  tournamentId: Id;
   shieldUrl?: string;
 
   private constructor(params: TeamAttributes) {
     super(params.id, new Date());
     this.name = params.name;
     this.shieldUrl = params.shieldUrl;
+    this.tournamentId = params.tournamentId;
   }
 
   static build(params: TeamAttributes): DomainEvent {
--- src/features/team/usecases/create-team/create-team.param.ts
+++ src/features/team/usecases/create-team/create-team.param.ts
@@ -1,4 +1,6 @@
+import { Id } from "../../../../core";
 
 export interface CreateTeamParam {
   name: string;
+  tournamentId: Id;
 }
