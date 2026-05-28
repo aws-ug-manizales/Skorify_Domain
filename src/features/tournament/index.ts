5f25c8205b42d24499d0588bf6fffe71519ce78d
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2026-05-28 18:21:57 -0500

    Update

--- src/features/tournament/index.ts
+++ src/features/tournament/index.ts
@@ -1,18 +1,24 @@
-export { MatchType, TournamentAttributes, TournamentEntity } from './tournament.entity';
+export {
+  MatchType,
+  TournamentAttributes,
+  TournamentEntity,
+} from "./tournament.entity";
 
-export * from './domain-events';
+export * from "./domain-events";
 
-export { TournamentContract } from './tournament.contract';
+export { TournamentContract } from "./tournament.contract";
 
-export { CreateTournamentParam } from './usecases/create-tournament/create-tournament.param';
-export { CreateTournamentUsecase } from './usecases/create-tournament/create-tournament.usecase';
+export { CreateTournamentParam } from "./usecases/create-tournament/create-tournament.param";
+export { CreateTournamentUsecase } from "./usecases/create-tournament/create-tournament.usecase";
 
-export { GetTournamentByIdParam } from './usecases/get-tournament-by-id/get-tournament-by-id.param';
-export { GetTournamentByIdUsecase } from './usecases/get-tournament-by-id/get-tournament-by-id.usecase';
+export { GetTournamentByIdParam } from "./usecases/get-tournament-by-id/get-tournament-by-id.param";
+export { GetTournamentByIdUsecase } from "./usecases/get-tournament-by-id/get-tournament-by-id.usecase";
 
-export { FilterTournamentsParam } from './usecases/filter-tournaments/filter-tournaments.param';
-export { FilterTournamentsUsecase } from './usecases/filter-tournaments/filter-tournaments.usecase';
+export { FilterTournamentsParam } from "./usecases/filter-tournaments/filter-tournaments.param";
+export { FilterTournamentsUsecase } from "./usecases/filter-tournaments/filter-tournaments.usecase";
 
-export { UpdateTournamentParam } from './usecases/update-tournament/update-tournament.param';
-export { UpdateTournamentUsecase } from './usecases/update-tournament/update-tournament.usecase';
+export { UpdateTournamentParam } from "./usecases/update-tournament/update-tournament.param";
+export { UpdateTournamentUsecase } from "./usecases/update-tournament/update-tournament.usecase";
 
+export { GetAvailableTournamentsParam } from "./usecases/get-available-tournaments/get-available-tournaments.param";
+export { GetAvailableTournamentsUsecase } from "./usecases/get-available-tournaments/get-available-tournaments.usecase";
---
+++ src/features/tournament/usecases/get-available-tournaments/get-available-tournaments.usecase.ts
@@ -0,0 +1,4 @@
+import { Usecase } from "../../../../core/usecase";
+import { GetAvailableTournamentsParam } from "./get-available-tournaments.param";
+
+export abstract class GetAvailableTournamentsUsecase extends Usecase<GetAvailableTournamentsParam> {}
