070f6abb273c158b0bc8629971f0cea82c9d3d06
bryanArroyave <bryanarroyaveo@gmail.com>
2026-05-29 10:59:34 -0500

    Add get-global-tournament-instance

---
+++ src/features/tournament-instance/usecases/get-global-tournament-instance/get-global-tournament-instance.usecase.ts
@@ -0,0 +1,4 @@
+import { Usecase } from '../../../../core/usecase';
+import { GetGlobalTournamentInstanceParam } from './get-global-tournament-instance.param';
+
+export abstract class GetGlobalTournamentInstanceUsecase extends Usecase<GetGlobalTournamentInstanceParam> {}
