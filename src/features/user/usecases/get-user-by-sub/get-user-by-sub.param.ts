f28c8897c4afa895a71d792d483f5bea9cf3d34b
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2026-06-01 10:57:43 -0500

    Update

---
+++ src/features/user/usecases/get-user-by-sub/get-user-by-sub.param.ts
@@ -0,0 +1,3 @@
+export interface GetUserBySubParam {
+  sub: string;
+}
---
+++ src/features/user/usecases/get-user-by-sub/get-user-by-sub.usecase.ts
@@ -0,0 +1,4 @@
+import { Usecase } from "../../../../core/usecase";
+import { GetUserBySubParam } from "./get-user-by-sub.param";
+
+export abstract class GetUserBySubUsecase extends Usecase<GetUserBySubParam> {}
