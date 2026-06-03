8164d000d0b29c0cc8099d0cd4c78674be1e181d
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2026-06-03 15:20:16 -0500

    Update

--- src/features/user/index.ts
+++ src/features/user/index.ts
@@ -12,6 +12,9 @@ export { GetUsersByIdsUsecase } from "./usecases/get-user-by-ids/get-users-by-id
 export { GetUserBySubParam } from "./usecases/get-user-by-sub/get-user-by-sub.param";
 export { GetUserBySubUsecase } from "./usecases/get-user-by-sub/get-user-by-sub.usecase";
 
+export { GetAvailableUsersParam } from "./usecases/get-available-users/get-available-users.param";
+export { GetAvailableUsersUsecase } from "./usecases/get-available-users/get-available-users.usecase";
+
 export { RegisterUserParam } from "./usecases/register-user/register-user.param";
 export { RegisterUserUsecase } from "./usecases/register-user/register-user.usecase";
 
---
+++ src/features/user/usecases/get-available-users/get-available-users.usecase.ts
@@ -0,0 +1,4 @@
+import { Usecase } from "../../../../core/usecase";
+import { GetAvailableUsersParam } from "./get-available-users.param";
+
+export abstract class GetAvailableUsersUsecase extends Usecase<GetAvailableUsersParam> {}
